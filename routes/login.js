64efdc1517bdb09f03a94d38dae34099fc621e02
root <root@snf-793419>
2017-12-19 09:22:20 +0200

    scope fix

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,5 @@
 module.exports = (router, passport) => {
-    router.get('/login/itlogin', passport.authenticate('itlogin', {scope: ['cn, mail, id']}));
+    router.get('/login/itlogin', passport.authenticate('itlogin', {scope: ['cn,mail,id']}));
     router.get('/login/login/callback', function (req, res, next) {
         passport.authenticate('itlogin', function (err, user) {
             if (err || !user) {
