ebfd4f3fc0ac56c26b412983060cdfdb300ddd33
Dimitris Arampatzis <32837348+darampat@users.noreply.github.com>
2018-01-08 21:18:34 +0200

    Change comments from English to Greek

--- config/Webhose.js
+++ config/Webhose.js
@@ -6,10 +6,10 @@ const client = webhoseio.config({
 });
 
 const makeQuery = (keyword, type) => {
+    // Αποκλεισμός μερικών ιστοσελίδων απο τα αποτελέσματα
     return {
         'q': '(' + keyword + ') language:greek site_type:' + type + ' -site:kerkida.net -site:parapolitika.gr' +
         ' -site:mykosmos.gr -site:escortsmovies.gr -site:teokanistras.gr -site:presshub.gr -site:cosmosjobs.com',
-        //δουλεύει 'q': keyword + ' language:greek site_type:' + type,
         'sort': 'crawled',
         'size': 10
     };
